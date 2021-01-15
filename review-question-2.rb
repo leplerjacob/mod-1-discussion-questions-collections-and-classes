f56ed3da45b4af2e3aa363ca19d6286c6a97cf8d
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-15 14:24:59 -0500

    commit tree 4f23542b8491bc1b8322012e1cf69cae3df909e3

--- review-question-2.rb
+++ review-question-2.rb
@@ -2,7 +2,26 @@
 
 class Car
 
+    attr_reader :make, :model
 
+    @@all_cars = []
+
+    
+    def initialize(*args)
+        p args
+        binding.pry
+        # @make = make
+        # @model = model
+        # @@all_cars.push(self)
+    end
+
+    def self.drive
+        "VROOOOOOOOOOOOM!"
+    end
+
+    def self.all
+        @all_cars
+    end
 
 end
 
@@ -21,7 +40,7 @@ Car.drive
 Car.all
 #=> [#<Car:0x00007fae28930f20>, #<Car:0x00007fae28923370>, #<Car:0x00007fae2891ae78>]
 
-BONUS:
+# BONUS:
 
 volvo_lightning = Car.new(make: "Volvo", model: "Lightning")
 
