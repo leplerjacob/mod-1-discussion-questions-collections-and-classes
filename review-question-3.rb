72fd73791e40cb7abe6c47a06175234ef17c6f5b
Jacob Lepler <lepler.jacob@gmail.com>
2021-01-16 00:53:51 -0500

    completed 1 & 2

--- review-question-3.rb
+++ review-question-3.rb
@@ -1,22 +1,56 @@
 # begin to build a simple program that models Instagram
 # you should have a User class, a Photo class and a comment class
+require 'pry'
 
+class User
+    attr_reader :name
+    
+    def initialize(name)
+        @photos = []
+        @name = name
+    end
 
-sandwich_photo = Photo.new
-sophie = User.new("Sophie")
-sandwich_photo.user = sophie
-sandwich_photo.user.name
-# => "Sophie"
-sophie.photos
+    def photos
+        @photos
+    end
+
+    def add_photos(photo)
+        @photos.push(photo)
+    end
+
+end
+
+class Photo
+
+    def user=(owner)
+        owner.add_photos(self)
+        @user = owner
+    end
+    
+    def user
+        @user
+    end
+    
+end
+
+puts "DONE: ", sandwich_photo = Photo.new
+puts "DONE: ", sophie = User.new('Sophie')
+puts "DONE: ", sandwich_photo.user = sophie
+puts "DONE: ", sandwich_photo.user.name 
+
+puts "NOT DONE"
+
+# binding.pry
+puts "Photos: ", sophie.photos
 # => [#<Photo:0x00007fae2880b370>]
 
-
-sandwich_photo.comments
+binding.pry
+p sandwich_photo.comments
 # => []
 
-sandwich_photo.make_comment("this is such a beautiful photo of your lunch!! I love photos of other people's lunch")
-sandwich_photo.comments
+p sandwich_photo.make_comment("this is such a beautiful photo of your lunch!! I love photos of other people's lunch")
+p sandwich_photo.comments
 # => [#<Comment:0x00007fae28043700>]
 
-Comment.all
+# p Comment.all
 #=> [#<Comment:0x00007fae28043700>]
